6fce83e2fe81421b6d49ab5bdb70389f33dc2373
Burak Albayrak <brkcanalbayrak@gmail.com>
2025-08-23 12:45:36 +0000

    daily_metrics created

---
+++ models/daily_metrics.sql
@@ -0,0 +1,55 @@
+{{ 
+    config(materialized='table') 
+}}
+
+
+WITH daily_metric_cte AS(
+SELECT
+    event_date,
+    country,
+    platform,
+    COUNT(DISTINCT user_id) AS DAU,
+    SUM(Iap_revenue) AS total_iap_revenue,
+    SUM(ad_revenue) AS total_ad_revenue,
+    SUM(match_start_count) AS matches_started,
+    SUM(victory_count) AS sum_victory_count,
+    SUM(defeat_count) AS sum_defeat_count,
+    SUM(match_end_count) AS sum_match_end_count,
+    SUM(server_connection_error) AS sum_server_connection_error
+
+    
+
+FROM
+    vertigo_user_raw.raw_merged
+GROUP BY
+    1, 2, 3
+ORDER BY
+    event_date,
+    country,
+    platform
+)
+
+SELECT
+    event_date,
+    country,
+    platform,
+    DAU,
+    total_iap_revenue,
+    total_ad_revenue,
+    matches_started,
+    matches_started / NULLIF(DAU, 0) AS match_per_dau,
+    sum_victory_count / NULLIF(sum_match_end_count, 0) AS win_ratio,
+    sum_defeat_count / NULLIF(sum_match_end_count, 0) AS defeat_ratio,
+    sum_server_connection_error / NULLIF(DAU, 0) AS server_error_per_dau
+    
+
+FROM
+    daily_metric_cte
+
+ORDER BY
+    event_date,
+    country,
+    platform
+
+
+
